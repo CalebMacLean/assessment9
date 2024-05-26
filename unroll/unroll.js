0d258990d412de1454b907c2d93872c58ef44b67
caleb <CMaclean98@gmail.com>
2024-05-25 17:20:13 -0700

    prompt 2 completed

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,31 @@
 function unroll(squareArray) {
+    let endResult = [];
 
+    // base case
+    if(squareArray.length === 0) {
+        return endResult
+    }
+
+    // top
+    endResult.push(...squareArray.shift());
+
+    // right
+    for (let i = 0; i < squareArray.length - 1; i++) {
+        endResult.push(squareArray[i].pop());
+    }
+
+    // bottom
+    if (squareArray.length !== 0) {
+        endResult.push(...squareArray.pop().reverse());
+    }
+
+    // left
+    for (let i = squareArray.length - 1; i >= 0; i--) {
+        endResult.push(squareArray[i].shift());
+    }
+
+    // recursive call
+    return endResult.concat(unroll(squareArray));
 }
 
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -6,4 +6,14 @@ describe("#unroll", function () {
     expect(typeof unroll).toEqual("function");
   });
 
+  it("unrolls inner arrays into the outer array", function() {
+    const squareArray = [
+      [1, 2, 3, 4],
+      [5, 6, 7, 8],
+      [9, 10, 11, 12],
+      [13, 14, 15, 16]
+    ];
+
+    expect(unroll(squareArray)).toEqual([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]);
+  })
 });
